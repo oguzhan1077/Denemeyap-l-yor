943a0789a11b40ad3ea096effdec44fde9203abe
Oguzhan Karakas <oguzhan.karakkas@gmail.com>
2020-05-11 04:28:27 +0300

    added new1 Python

---
+++ Pyton/new.py
@@ -0,0 +1,10 @@
+#a^2+b^2 = c^2 (Dik üçgenin hipotenüsünü bulma)
+
+
+        a = int(input("a kenarını girin:"))
+        b = int(input("b kenarını girin:"))
+
+       hipotenus = a**2 + b**2
+
+print(hipotenus**0.5)
+
